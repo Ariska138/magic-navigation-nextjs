ff96b12ed56ca434bf9728cd907233569f43814a
Ariska Hidayat <ariska138@gmail.com>
2025-02-22 14:26:35 +0700

    fix navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,11 +6,7 @@ import {
   PersonOutline,
   SettingsOutline,
 } from 'react-ionicons';
-import UserIcon from '@/icons/UserIcon';
-import styles from './styles.module.css';
 
-import Image from 'next/image';
-import PersonIcon from '../../public/assets/person.svg';
 const Navigation = () => {
   const Menus = [
     {
@@ -39,7 +35,9 @@ const Navigation = () => {
       dis: 'translate-x-64',
     },
   ];
+
   const [active, setActive] = useState(0);
+
   return (
     <div className="bg-white max-h-[4.4rem] px-6 rounded-t-xl">
       <ul className="flex relative">
@@ -59,39 +57,25 @@ const Navigation = () => {
         {Menus.map((menu, i) => (
           <li key={i} className="w-16">
             <a
-              className="flex flex-col text-center pt-6"
+              className="flex flex-col text-center pt-6 cursor-pointer"
               onClick={() => setActive(i)}
             >
               <span
-                className={`text-xl self-center z-10 cursor-pointer duration-500 ${
-                  i === active && '-mt-6 text-white'
+                className={`text-xl self-center z-10 duration-500 ${
+                  i === active ? '-mt-6 text-white' : ''
                 }`}
               >
-                {/* tidak bisa ganti warna */}
-                {/* <Image
-                  src={PersonIcon}
-                  alt={''}
-                  className={`${i === active && ' text-white'}`}
-                  style={{ color: i === active ? 'white' : 'black' }}
-                /> */}
-                {/* bisa gerjalan dengan baik */}
-                <UserIcon
-                  className={`h-5 w-5`}
-                  style={{
-                    fill: i === active ? 'white' : 'black',
-                    transition: 'fill 0.5s ease',
-                  }}
-                />
-                {/* tidak bisa melakukan transisi */}
-                {/* {React.cloneElement(menu.icon, {
+                {React.cloneElement(menu.icon, {
                   color: i === active ? 'white' : 'black',
-                  transition: 'fill 0.5s ease',
-                })} */}
+                  style: {
+                    transition: 'color 0.5s ease',
+                  },
+                })}
               </span>
               <span
-                className={` ${
+                className={`${
                   active === i
-                    ? 'translate-y-4 duration-700 opacity-100'
+                    ? 'translate-y-5 duration-700 opacity-100'
                     : 'opacity-0 translate-y-10'
                 } `}
               >
